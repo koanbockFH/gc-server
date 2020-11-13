30b09672c6f1a3bc22593f10bfde2378cc682c5d
koanbockFH <1810653162@stud.fh-kufstein.ac.at>
2020-11-13 18:00:33 +0100

    fix some bugs in module

--- src/common/decorator/commonApi.decorator.ts
+++ src/common/decorator/commonApi.decorator.ts
@@ -1,7 +1,10 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiInternalServerErrorResponse, ApiResponseOptions } from '@nestjs/swagger';
+import { ApiInternalServerErrorResponse, ApiOkResponse, ApiResponseOptions } from '@nestjs/swagger';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function ApiCommonResponse(options?: ApiResponseOptions): any {
-  return applyDecorators(ApiInternalServerErrorResponse({ description: 'Something is wrong!' }));
+  return applyDecorators(
+    ApiOkResponse(options),
+    ApiInternalServerErrorResponse({ description: 'Something is wrong!' }),
+  );
 }
